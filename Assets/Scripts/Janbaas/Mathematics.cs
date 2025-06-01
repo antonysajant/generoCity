acaebb121e96ab262fd7fed9c7911cae2cd5558c
Janbaas Jamal K K <janbaasjamal1@gmail.com>
2025-06-01 20:37:56 +0530

    IDK What to do

--- Assets/Scripts/Janbaas/Mathematics.cs
+++ Assets/Scripts/Janbaas/Mathematics.cs
@@ -9,6 +9,7 @@ public static class Mathematics
         velocity = currentVelocity + acceleration * Time.deltaTime;
         return velocity;
     } 
+<<<<<<< HEAD
     public static float ClosestMultiple(float value,float roundTo)
     {
         if (roundTo == 0)
@@ -20,5 +21,11 @@ public static class Mathematics
             return value - remainder;
         else
             return value + (roundTo - remainder);
+=======
+    public static float RotationCalculator(float currentRotation, float targetRotation, float rotationRatio)
+    {
+        float rotation = Mathf.LerpAngle(currentRotation, targetRotation, rotationRatio * Time.deltaTime);
+        return rotation;
+>>>>>>> parent of 79bc7a7 (Very close to haveing the car ai)
     }
 }
